b656ca3a2cf2a35ab39e31c91de16dcc8e28df85
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-02-15 14:03:04 -0600

    Added score attribute.

--- lib/Photo.dart
+++ lib/Photo.dart
@@ -3,8 +3,9 @@ class Photo {
   final String name;
   final String imageData;
   final int size;
+  final int score;
 
-  Photo({this.id, this.name, this.imageData, this.size});
+  Photo({this.id, this.name, this.imageData, this.size, this.score});
 
   factory Photo.fromJson(Map<String, dynamic> json) {
     return Photo(
@@ -12,6 +13,7 @@ class Photo {
       name: json['name'],
       imageData: json['photo'],
       size: json['size'],
+      score: json['score'],
     );
   }
 }
